80407ac88abdd9c94ad1ea2adb60c7da47874ed4
Michelle B <82473721+mchlbblv@users.noreply.github.com>
2022-09-01 17:44:28 -0400

    Implemented freeCodeCamp's pagination tutorial, next step to map data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,38 @@
-import React, { Component }  from 'react';
+import React, { useState, useMemo }  from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as meteorData from './call.js'
+import {meteorData, makeCall} from './call.js'
+import Pagination from './Pagination';
+
+let PageSize = 25;
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    console.log("meteorData from App.js: ", meteorData);
+    return meteorData.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
   return (
     <div className="App">
-      <button type = "button" onClick ={() => meteorData.makeCall()}>Click to get data</button>
+      <button type = "button" onClick ={() => makeCall()}>Click to get data</button>
+  
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={meteorData.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />
+
       <section className = "data-grid">
       
       </section>
     </div>
+
   );
 }
 
--- react-app/src/call.js
+++ react-app/src/call.js
@@ -1,6 +1,8 @@
 import './App';
+
 const url = `https://data.nasa.gov/resource/gh4g-9sfh.json`
-let meteorName, id, nametype, recclass, mass, fall, year, reclat, reclong, meteorText, geolocation, lat, long;
+let meteorData, meteorName, id, nametype, recclass, mass, fall, year, reclat, reclong, meteorText, geolocation, lat, long;
+
 async function makeCall() {
     const xhr = new XMLHttpRequest();
     xhr.onload = handleResponse;
@@ -15,27 +17,31 @@ async function handleResponse(e) {
      * populated with currently meteorText
     */
     let dataGrid = document.querySelector(".data-grid"); 
-    const data = e.target.responseText;
-    const dataObject = JSON.parse(data);
+    const data = e.target.response;
+    // const dataObject = JSON.parse(data);
+    meteorData = JSON.parse(data);
+    console.log("meteorData from call.js: ", meteorData);
+
     let counter = 0;
-    dataObject.forEach(obj => {
-        
+    meteorData.forEach( obj => {
+        counter += 1;
         meteorName = obj.name;
         id = obj.id;
         nametype = obj.nametype;
         recclass = obj.recclass;
-        mass = obj.mass;
-        mass = convert(mass);
+        if (obj.mass === undefined) {
+            mass = "Mass Unknown";
+        }
+        else {
+            mass = convert(obj.mass);
+        }
         fall = obj.fall;
         if(obj.year === undefined) {
              year = "Year Unknown";
         } else {
             year = obj.year;
-            console.log("OOGA BOOGA object year: " + obj.year);
             year = year.slice(0, 4);
         }
-        
-       
 
         reclat = obj.reclat;
         reclong = obj.reclong;
@@ -50,8 +56,11 @@ async function handleResponse(e) {
         
         // adds content to dataGrid
         dataGrid.innerHTML += meteorText;
-        
+        if (counter === 100) {
+            return;
+        }
     })
+    return meteorData;
 }
 
 /*
@@ -61,7 +70,7 @@ async function handleResponse(e) {
 const convert = (num) => {
     // num, from mass, is inputted
     // as a string from the JSON
-    let temp = parseFloat(num);
+    let temp = num.toString();
 
     let lgth = temp.length;
     // console.log(lgth);
@@ -81,6 +90,7 @@ const convert = (num) => {
 
 // to access in other files
 export {
+    meteorData,
     makeCall
 }
 
