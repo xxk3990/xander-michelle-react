bf38efaa3e2cebaa08a4d76db630ede19f5ab29b
Michelle B <82473721+mchlbblv@users.noreply.github.com>
2022-09-04 16:18:12 -0400

    added comments, resources, troubleshooting yielded no hope

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,119 +1,112 @@
+/* 
+ * filename - App.js
+ * resources: 
+   1. https://www.freecodecamp.org/news/build-a-custom-pagination-component-in-react/
+ */
+
 import React, { useState, useMemo }  from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 // import {meteorData, makeCall} from './call.js'
 import Pagination from './Pagination';
 
 let PageSize = 25;
 // let  meteorName, id, nametype, recclass, mass, fall, year, reclat, reclong, meteorText, geolocation, lat, long;
+
 function App() {
-  const url = `https://data.nasa.gov/resource/gh4g-9sfh.json`
-  window.onload = () => {
-    makeCall();
-  }
+
+  window.onload = () => makeCall;
+  console.log("h"); // this is getting logged twice uh
+
+  // Global variable to be populated
+  // with the meteor data from the JSON
   let meteorData;
+
+  /* 
+   * makeCall() - HTTP request
+   * retrieves info from external JSON
+   */
   const makeCall = () => {
+    const url = `https://data.nasa.gov/resource/gh4g-9sfh.json`;
     const xhr = new XMLHttpRequest();
-    xhr.onload = (e) => {
-      const data = e.target.response;
-      console.log("data from API: " + data)
-      meteorData = JSON.parse(data);
-      return meteorData;
-    }
+    xhr.onload = handleResponse;
     xhr.open('GET', url);
     xhr.send();
   }
-  // const handleResponse = (e) => {
-    
-  // }
+
+  /* 
+   * handleResponse() - where 
+   * data from the JSON is parsed
+   */
+  const handleResponse = (e) => {
+    const data = e.target.response;
+    console.log("data from API: " + data); // NOT GETTING THIS CALL IN CONSOLE EVEN ?????
+    meteorData = JSON.parse(data);
+  }
+
+  // for pagination, from freecodecamp
+  // at top of page as resource 1
   const [currentPage, setCurrentPage] = useState(1);
-  if(meteorData === undefined) {
-    return console.log("data is undefined");
+
+  // data existence validation 
+  if(meteorData === undefined) { // THIS IS WHERE CODE STOPS, NEVER HITS ELSE
+    // CONTINUES THINKING METEORDATA IS UNDEFINED :(
+    // also, printing things in the console twice?
+    return console.log("data is undefined"); // ONLY CONSOLE LOG THAT WORKS ATM
   } else {
-    console.log(meteorData);
-    const currentMeteorData = useMemo(() => {
+    console.log("meteorData from else, \nAfter data validation: ", meteorData);
     
+    const currentMeteorData = useMemo(() => {
       const firstPageIndex = (currentPage - 1) * PageSize;
       const lastPageIndex = firstPageIndex + PageSize;
       return meteorData.slice(firstPageIndex, lastPageIndex);
+    }, [currentPage]);
+
+    console.log("OOGA BOOGA current meteor data: " + currentMeteorData); // NOT HITTING
     
-  }, [currentPage]);
-  console.log("OOGA BOOGA current meteor data: " + currentMeteorData)
-  if((meteorData === undefined) || (meteorData.length == 0)) {
-    return (
-        <div className="App">
-          No meteor data, sad face
-        </div>
-    )
-  } else {
-    console.log("meteorData from App.js: ", meteorData);
-    
-    return (
-      <div className="App">
-        
-    
-        <Pagination
-          className="pagination-bar"
-          currentPage={currentPage}
-          totalCount={meteorData !== undefined ? meteorData.length : 0}
-          pageSize={PageSize}
-          onPageChange={page => setCurrentPage(page)}
-        />
+    if((meteorData === undefined) || (meteorData.length === 0)) {
+      return (
+          <div className="App">
+            No meteor data, sad face
+          </div>
+      )
+    } else {
+      console.log("meteorData from App.js: ", meteorData); // NOT HIT
       
-        <section className = "data-grid">
-         {currentMeteorData.map(m => {
-          return (
-            <section class="meteorText">
-                              <h3 id = "meteor-name">{m.name}</h3>
-                              <p>{m.id}</p>
-                              <p>Coordinates: {m.reclat}, {m.reclong}</p> 
-                              <p>Year: {m.year}</p>
-                              <p>Mass: {m.mass}</p>
-                          </section>
-          )
-         })}
-        </section>
-      </div>
-  
-    );
-  }
-  }
-  
-
-
-  // if(meteorData == null || meteorData.length === 0) {
-  //   return (
-  //     <div className='App'>
-  //       No meteor data available, sad face
-  //     </div>
-  //   )
-  // } else {
-
-  // }
-  
-}
-
-
-
-
-// async function makeCall() {
-    
-// }
-
-// parse data
-// async function handleResponse(e) {
-//     /* 
-//      * dataGrid - required at the beginning, gets 
-//      * populated with currently meteorText
-//     */
-//     // let dataGrid = document.querySelector(".data-grid"); 
-    
-       
-  
+      return (
+        /* Hook for pagination from resource 1 */
+        <div className="App">
+          <Pagination
+            className="pagination-bar"
+            currentPage={currentPage}
+            totalCount={meteorData !== undefined ? meteorData.length : 0}
+            pageSize={PageSize}
+            onPageChange={page => setCurrentPage(page)}
+          />
         
-// }
-
-//window.onload = () => makeCall();
+          {/* 
+            * HTML for displaying meteor data 
+            * (used to work lol until pagination)
+          */}
+          <section className = "data-grid">
+          {currentMeteorData.map(m => {
+            return (
+              <section class="meteorText">
+                <h3 id = "meteor-name">{m.name}</h3>
+                <p>{m.id}</p>
+                <p>Coordinates: {m.reclat}, {m.reclong}</p> 
+                <p>Year: {m.year}</p>
+                <p>Mass: {m.mass}</p>
+              </section>
+            );
+          })}
+          </section> 
+        </div>
+      );
+    } // inner else
+  } // outer else 
+  
+} // App
 
 /*
  * convert() adds proper suffix based 
