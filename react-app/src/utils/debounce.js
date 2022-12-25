0227c4cd83628dee383d179516f348a4048349c9
Peter Krieg <pkrieg2@gmail.com>
2022-12-25 09:46:29 -0500

    simplify debounce function a bit

--- react-app/src/utils/debounce.js
+++ react-app/src/utils/debounce.js
@@ -1,13 +1,11 @@
-export function debounce(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this,
-      args = arguments;
-    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    }, wait);
-    if (immediate && !timeout) func.apply(context, args);
-  };
+function debounce(func, duration) {
+  let timeout
+  return function (...args) {
+    const effect = () => {
+      timeout = null
+      return func.apply(this, args)
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(effect, duration)
+  }
 }
